8cc43e49b45330782f9e2d3dafe5c47054b648eb
Marin Bilan <marinbilan@gmail.com>
2022-04-13 23:14:11 +0200

    Commit 7 - Tested some iterators --------------------------------
    Updated UT
    
    MB

--- src/Mod-1/unitTest/unitTest.cpp
+++ src/Mod-1/unitTest/unitTest.cpp
@@ -76,6 +76,108 @@ TEST_F(myClassTest, mockUnitTest)
 
 }
 
+
+class Widget
+{
+public:
+	Widget(int id) : m_id(id) {}
+
+	int getId() const
+	{
+		return m_id;
+	}
+
+private:
+int m_id;
+};
+
+
+TEST_F(myClassTest, TestVectorIterators)
+{
+	// ---- Basic iterator operations ----
+	std::vector<int> vecOfInt = {0, 2, 16, 8, 11};
+
+	std::vector<int>::iterator it0 = vecOfInt.begin();
+	for(; it0 != vecOfInt.end(); it0++)
+	{
+		// Iterator address is always the same - only value is increased
+		std::cout << "it0 address: " << &it0 << " vec elem address: " << &(*it0) << " vec elem value: " << *it0 << '\n';
+	}
+
+	// Return iterator on first element greather than 10
+	// it1 will have own address, but underlying elem of vector will have the same address and value
+	std::vector<int>::iterator it1 = std::find_if(vecOfInt.begin(), vecOfInt.end(), 
+		[=](int elemInFoo){ return  (elemInFoo >= 10); });
+	std::cout << '\n' << "it1 address: " << &it1 << " vec elem address: " << &(*it1) << " vec elem value: " << *it1 << '\n';
+
+	for(const auto& s : vecOfInt) // Important: auto is int NOT iterator!
+	{
+		std::cout << "Address of elem in vec: " << &s << " value of elem in vec: " << s << '\n';
+	}
+
+	// ---- Basic filtering ----
+	std::vector<int> vecOfFilteredElems;
+
+    // copy only positive numbers:
+    int upper = 11;
+    int low = 2;
+    std::copy_if(vecOfInt.begin(), vecOfInt.end(), std::back_inserter(vecOfFilteredElems), 
+    	[=](int elemInFoo){ return  (elemInFoo >= low) && (elemInFoo <= upper); });
+
+	for(const auto& s : vecOfFilteredElems) // Important: auto is int NOT iterator!
+	{
+		std::cout << "Filtered elem: " << s << '\n';
+	}
+
+
+	// ---- Test vector of shared pointers ----
+	std::shared_ptr<Widget> p0 = std::make_shared<Widget>(17);
+	std::shared_ptr<Widget> p1 = std::make_shared<Widget>(213);
+	std::shared_ptr<Widget> p2 = std::make_shared<Widget>(19);
+	std::shared_ptr<Widget> p3 = std::make_shared<Widget>(1);
+
+	std::vector<std::shared_ptr<Widget>> vecOfWidgetPtrs {p0, p1, p2, p3};
+
+	// By dereferencing iterator (*) - access underlying shared pointer in vector
+	std::vector<std::shared_ptr<Widget>>::iterator it2 = vecOfWidgetPtrs.begin();
+	for(; it2 != vecOfWidgetPtrs.end(); it2++)
+	{
+		std::cout << (*it2)->getId() << '\n';
+	}
+
+	// auto is type of std::shared_ptr<Widget> - NOT iterator! (Direct access)
+	for(auto const& s : vecOfWidgetPtrs)
+	{
+		std::cout << "Direct access: " << s->getId() << '\n';
+	}
+
+	// ---- Sort ----
+    std::sort(vecOfWidgetPtrs.begin(), vecOfWidgetPtrs.end(), 
+    	[](const std::shared_ptr<Widget>& a, const std::shared_ptr<Widget>& b) // -> int
+    {
+        return a->getId() < b->getId();
+    });
+
+	std::cout << '\n';
+	// auto is type of std::shared_ptr<Widget> - NOT iterator! (Direct access)
+	for(auto const& s : vecOfWidgetPtrs)
+	{
+		std::cout << "Sorted: " << s->getId() << '\n';
+	}
+
+	// Filter - do something with filtered shared ptr Widgets
+	int param = 15;
+	//
+	std::cout << '\n';
+    for_each(vecOfWidgetPtrs.begin(),vecOfWidgetPtrs.end(),[=](std::shared_ptr<Widget> const& p)
+    {
+    	// Here p is shared_ptr Widget - NOT iterator!
+        if( p->getId() > param ) std::cout << "Filter print:" << p->getId() << '\n';
+    });
+
+}
+
+
 /*
 int main(int argc, char** argv)
 {
