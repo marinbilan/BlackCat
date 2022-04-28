ce1872b16a108560f2d97db84b5ea8e133ac8427
Marin Bilan <marinbilan@gmail.com>
2022-04-28 23:14:00 +0200

    Commit 12 - Implemented HTTP Client
    MB

--- src/Mod-1/src/bcMain.cpp
+++ src/Mod-1/src/bcMain.cpp
@@ -1,4 +1,9 @@
+#include <fstream>
+#include <functional>
 #include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
 
 #include "FactoryIf.h"
 #include "Factory.h"
@@ -12,141 +17,216 @@
 #include <asio/ts/buffer.hpp>
 #include <asio/ts/internet.hpp>
 
+using namespace std::placeholders;
 
-/*
-Because we do not know how many data to get in buffer, create
-buffer with some size
-*/
-std::vector<char> vBuffer(20 * 1024);
+class HttpClient {
+public:
+    HttpClient(asio::io_service& io_service, asio::ip::tcp::resolver& resolver,
+               const std::string& host, const std::string& path)
+        : host_(host), path_(path), resolver_(resolver), sock_(io_service) { }
 
-void GrabSomeData(asio::ip::tcp::socket& socket)
+
+    void Start() {
+        // The client must start by resolving the hostname into an IP endpoint.
+        // This will give us a destination for the TCP connection. We can
+        // safely hard code the "http" service name.
+        resolver_.async_resolve(
+            asio::ip::tcp::resolver::query(host_, "http"),
+            [this](std::error_code ec,
+                   asio::ip::tcp::resolver::iterator it) {
+                if (ec) {
+                    std::cout << "Error resolving " << host_ << ": "
+                    	<< ec.message();
+                    return;
+                }
+
+                // For simplicity, we'll assume the first endpoint will always
+                // be available.
+                std::cout << host_ << ": resolved to " << it->endpoint()
+                          << std::endl;
+
+                do_connect(it->endpoint());
+            });
+    }
+
+
+private:
+    void do_connect(const asio::ip::tcp::endpoint& dest) {
+        // Remember that the Asio library will make copies of parameters passed
+        // by const reference, so it's ok to let the endpoint go out of scope
+        // when this method returns.
+        sock_.async_connect(
+            dest, [this](std::error_code ec) {
+                if (ec) {
+                    std::cout << "Error connecting to " << host_ << ": "
+                               << ec.message();
+                    return;
+                }
+
+                std::cout << host_ << ": connected to "
+                          << sock_.remote_endpoint() << std::endl;
+                do_send_http_get();
+            });
+    }
+
+
+    void do_send_http_get() {
+        // At minimum, the remote server needs to know the path being fetched
+        // and the host serving that path. The latter is required because a
+        // single server often hosts multiple domains.
+        request_ = "GET " + path_ + " HTTP/1.1\r\nHost: " + host_ + "\r\n\r\n";
+        asio::async_write(
+            sock_, asio::buffer(request_),
+            [this](std::error_code ec, std::size_t size) {
+                if (ec) {
+                    std::cout << "Error sending GET " << ec;
+                    return;
+                }
+
+                std::cout << host_ << ": sent " << size << " bytes";
+                do_recv_http_get_header();
+            });
+    }
+
+
+    void do_recv_http_get_header() {
+        // Since HTTP/1.1 is a text based protocol, most of it is human readable
+        // by design. Notice how the "double end of line" character sequence
+        // ("\r\n\r\n") is used to delimit message sections.
+        asio::async_read_until(
+            sock_, response_, "\r\n\r\n",
+            [this](std::error_code ec, std::size_t size) 
+            {
+                if (ec) 
+                {
+                    std::cout << "Error receiving GET header " << ec;
+                    return;
+                }
+
+                std::cout<< host_ << ": received " << size << ", streambuf "
+                          << response_.size() << '\n';
+
+                // The asio::streambuf class can use multiple buffers
+                // internally, so we need to use a special iterator to copy out
+                // the header.
+                std::string header(
+                    asio::buffers_begin(response_.data()),
+                    asio::buffers_begin(response_.data()) + size);
+                response_.consume(size);
+
+                std::cout << "----------" << std::endl << host_
+                          << ": header length " << header.size() << std::endl
+                          << header;
+
+                // First we'll check for the explicit "Content-Length" length
+                // field. This provides the exact body length in bytes.
+                size_t pos = header.find("Content-Length: ");
+                if (pos != std::string::npos) {
+                    size_t len = std::strtoul(
+                        header.c_str() + pos + sizeof("Content-Length: ") - 1,
+                        nullptr, 10);
+                    do_receive_http_get_body(len - response_.size());
+                    return;
+                }
+
+                // The other alternative is a chunked transfer. There is a quick
+                // way to determine the remaining length in this case.
+                pos = header.find("Transfer-Encoding: chunked");
+                if (pos != std::string::npos) {
+                    do_receive_http_get_chunked_body();
+                    return;
+                }
+
+                std::cout << "Unknown body length" << '\n';
+            });
+    }
+
+
+
+    void do_receive_http_get_body(size_t len) {
+        // For "Content-Length" we know exactly how many bytes are left to
+        // receive.
+        asio::async_read(
+            sock_, response_, asio::transfer_exactly(len),
+            std::bind(&HttpClient::handle_http_get_body, this, _1, _2));
+    }
+
+
+    void do_receive_http_get_chunked_body() {
+        // For chunked transfers the final body chunk will be terminated by
+        // another "double end of line" delimiter.
+        asio::async_read_until(
+            sock_, response_, "\r\n\r\n",
+            std::bind(&HttpClient::handle_http_get_body, this, _1, _2));
+    }
+
+    
+    void handle_http_get_body(std::error_code ec,
+                              std::size_t size) {
+        if (ec) {
+            std::cout << "Error receiving GET body " << ec;
+            return;
+        }
+
+        std::cout << host_ << ": received " << size << ", streambuf "
+                  << response_.size();
+
+        // We can finally consume the body and print it out if desired.
+        const auto& data = response_.data();
+        std::string body(asio::buffers_begin(data), asio::buffers_end(data));
+        response_.consume(size);
+
+        std::cout << "----------" << std::endl << host_ << ": body length "
+                  << body.size() << std::endl;
+        
+        // Just console flag in program running args
+        //if (FLAGS_print_body) {
+            std::cout << body;
+        //}
+    }
+
+    const std::string host_;
+    const std::string path_;
+
+    asio::ip::tcp::resolver& resolver_;
+    asio::ip::tcp::socket sock_;
+
+    std::string request_;
+    asio::streambuf response_;
+};
+
+
+void fHTTPClient() 
 {
-	socket.async_read_some(asio::buffer(vBuffer.data(), vBuffer.size()), 
-		[&](std::error_code ec, std::size_t length)
-		{
-			if(!ec)
-			{
-				std::cout << "\n\nRead " << length << " bytes\n\n";
+	// Two expected fields: "host" and "path"
+	std::string host_0("www.google.com");
+	std::string path_0("/");
 
-				for(int i = 0; i < length; i++)
-					std::cout << vBuffer[i];
+	std::string host_1("jservice.io");
+	std::string path_1("/api/random");
+	// Prepare arguments for HTTP client
+	asio::io_service io_service;
+	asio::ip::tcp::resolver resolver(io_service);
+	std::vector<std::unique_ptr<HttpClient>> clients;
 
-				GrabSomeData(socket); 
-			}
-		}
-	);
+	// We are creating a new HttpClient for entry
+	std::unique_ptr<HttpClient> c(new HttpClient(io_service, resolver, host_0, path_0));
+	c->Start();
+
+	clients.push_back(std::move(c));
+
+	io_service.run();
 }
+
+
 
 //
 int main()
 {
-	// Create Factory
-	Common::Factory::getInstance();
-
-	// Move to Unit Test
-	Service::Service0 srv0_0("dbPath", "instName");
-	
-	// Test Factroy
-	srv0_0.testFactory();
-
-	// Set FactroyImpl
-	srv0_0.setFactoryImplPtr(Common::FactoryIf::getInstance());
-
-	// Test FactoryImpl
-	srv0_0.testFactoryImpl();
-
-
-
-	// ---- NETWORKING ----
-	asio::error_code ec;
-
-	// Create a "context" - The platform specific interface
-	asio::io_context context;
-
-	// Give some fake tasks to asio so the context doesnt finish
-	asio::io_context::work idleWork(context);
-
-	// Start the context
-	std::thread thrContext = std::thread([&] () { context.run(); });
-
-	// Get address of somewhere we wish to connect to
-	asio::ip::tcp::endpoint endpoint(asio::ip::make_address("93.184.216.34", ec), 80);
-
-	// Create a socket, the context will deliver the implementation
-	asio::ip::tcp::socket socket(context);
-
-	// Tell socket to try and connect
-	socket.connect(endpoint, ec);
-
-	if(!ec)
-	{
-		std::cout << "Connected!" << '\n';
-	} 
-	else 
-	{
-		std::cout << "Failed to connect to address:\n" << ec.message() << '\n';
-	}
-
-	if(socket.is_open())
-	{
-		// We need to wait until data is read
-		// CASE 1 : This is bad - just wait for response for example purpose
-		/* 
-		std::string sRequest = 
-		"GET /index.html HTTP/1.1\r\n"
-		"Host: example.com\r\n"
-		"Connection: close\r\n\r\n";
-
-		socket.write_some(asio::buffer(sRequest.data(), sRequest.size()), ec);
-
-
-		using namespace std::chrono_literals;
-		std::this_thread::sleep_for(200ms);
-
-		// CASE 2 : We can block thread and wait until some data are read
-		// If we want to get more data, we will get only part of that data
-
-		/*
-		socket.wait(socket.wait_read);
-
-		size_t bytes = socket.available();
-		std::cout << "Bytes Available: " << bytes << '\n';
-
-		if(bytes > 0)
-		{
-			std::vector<char> vBuffer(bytes);
-			// When we use sincronius reading - we use read_some
-			// socket.read_some(asio::buffer(vBuffer.data(), vBuffer.size()), ec);
-
-			// In asyncronius reading - use GramSomeData - async_read_some
-
-			for(auto c : vBuffer)
-			{
-				std::cout << c;
-			}
-		}
-		*/
-
-		// CASE 3 - Grab some data async.
-		// First start reading data before any request (data) is send to server
-		GrabSomeData(socket);
-
-		std::string sRequest = 
-			"GET /index.html HTTP/1.1\r\n"
-			"Host: example.com\r\n"
-			"Connection: close\r\n\r\n";
-
-		socket.write_some(asio::buffer(sRequest.data(), sRequest.size()), ec);
-
-		// Program does something else, while asio handles data transfer in background
-		using namespace std::chrono_literals;
-		std::this_thread::sleep_for(20000ms);
-
-		context.stop();
-		if(thrContext.joinable()) thrContext.join();
-	}
-
-
+	// HTTP client
+	// https://www.youtube.com/watch?v=lQx5UYQRZ34&ab_channel=BrianOrr
+	// https://github.com/v1bri/codecast/blob/master/006/http_client_main.cc
+	fHTTPClient();
 
 	return 0;
 }
