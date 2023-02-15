fcd94b233c5c127bf5bb8a31f3b7e573997e010e
Marin Bilan <marinbilan@gmail.com>
2023-02-16 00:15:27 +0100

    Commit 28 - Getting fin data done

--- sw/Mod-App-1/services/Networking/unitTest/unitTestNetworking.cpp
+++ sw/Mod-App-1/services/Networking/unitTest/unitTestNetworking.cpp
@@ -34,7 +34,265 @@ public:
 };
 
 
-TEST_F(NetworkingUnitTest, firstMyClassTest)
+/*
+[INCOME STATEMENT]
+
+	1] Total Revenue [Exists]
+	2] Gross Profit [Exists] - Revenue - Const of Revenue
+	3] Net Income Common Stockhold [Exists] - should be consistent
+	----
+
+	Gross Margin [%] = Gross Profit / Revenue
+	Net Margin [%] = Net Income / Revenue
+*/
+TEST_F(NetworkingUnitTest, DISABLED_getFromIncomeStatementTest)
+{
+	// [1] Preparation
+	// ----
+	// Create client and get Income Statement (HTML) in _HTTPSContent.txt file
+	boost::asio::ssl::context ctx(boost::asio::ssl::context::sslv23);
+	ctx.set_default_verify_paths();
+
+    boost::asio::io_service io_service;
+
+    // Income Statement- APPLE
+    std::string server("finance.yahoo.com");
+    std::string path("/quote/AAPL/financials?p=AAPL");
+
+	Networking::HTTPSClient c(io_service, ctx, server, path);
+    io_service.run();
+    // ----
+
+
+    // [2] Get: Revenue, Gross Profit Net Income Common Stackhold
+    // ----
+	std::string _fileName("_HTTPSContent.txt");
+
+	// Test regex in file
+	std::ifstream file(_fileName);
+
+	std::string lineFromFile;
+	std::smatch match;
+
+	std::regex regexRevenueLine("Total Revenue.*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*)");
+	std::regex regexGrossProfitLine("Gross Profit.*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*)");
+	std::regex regexNetIncomeLine("Net Income Common Stockholders.*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*)");
+
+	//
+	while (std::getline(file, lineFromFile)) 
+	{
+		// Get Total Revenue
+		if (std::regex_search(lineFromFile, match, regexRevenueLine))
+		{
+			for (size_t i = 1; i < match.size(); ++i)
+		    {
+			    std::string totalRevenueStr(match[i].str());
+			    // Remove "," from data
+			    totalRevenueStr.erase(remove(totalRevenueStr.begin(), totalRevenueStr.end(), ','), totalRevenueStr.end());
+
+			    double num = stod(totalRevenueStr);
+			    std::cout << "Total Revenue: " << num << '\n';
+			    // Push back in vector
+		    }
+		}
+		// Get Gross Profit
+		if (std::regex_search(lineFromFile, match, regexGrossProfitLine))
+		{
+			for (size_t i = 1; i < match.size(); ++i)
+		    {
+			    std::string grossProfitStr(match[i].str());
+			    // Remove "," from data
+			    grossProfitStr.erase(remove(grossProfitStr.begin(), grossProfitStr.end(), ','), grossProfitStr.end());
+
+			    double num = stod(grossProfitStr);
+			    std::cout << "Gross Profit: " << num << '\n';
+			    // Push back in vector
+		    }
+		}
+		// Get Net Income
+		if (std::regex_search(lineFromFile, match, regexNetIncomeLine))
+		{
+			for (size_t i = 1; i < match.size(); ++i)
+		    {
+			    std::string netIncomeStr(match[i].str());
+			    // Remove "," from data
+			    netIncomeStr.erase(remove(netIncomeStr.begin(), netIncomeStr.end(), ','), netIncomeStr.end());
+
+			    double num = stod(netIncomeStr);
+			    std::cout << "Net Income: " << num << '\n';
+			    // Push back in vector
+			    if(num != 0) {};
+		    }
+		}
+	}
+
+	// Close file
+	file.close();
+}
+
+
+/*
+[ BALANCE SHEET ]
+
+	4] Retained Earnings - Added to or widraw (If company invest - Growth)
+	5] Total Equity Gross Minority Interest [Equity]
+
+	To measure how efficient company use this reinvestment (Retained Earnings) we calc:
+	Return on Equity [%] = Net Income / Total Equity
+
+	----
+	Return on Net Tangible assets - More sofisticated method of how efficient
+	company use retained earnings (Book Esseys of Warren Buffett)
+	----
+
+	Look to bussiness with no Long Term Debt
+	6] Total Debt
+	If company can pay whole Total Debt with 4 years Earnings, that is good position
+*/
+TEST_F(NetworkingUnitTest, DISABLED_getFromBalanceSheetTest)
+{
+	// [1] Preparation
+	// ----
+	// Create client and get Income Statement (HTML) in _HTTPSContent.txt file
+	boost::asio::ssl::context ctx(boost::asio::ssl::context::sslv23);
+	ctx.set_default_verify_paths();
+
+    boost::asio::io_service io_service;
+
+    // Income Statement- APPLE
+    std::string server("finance.yahoo.com");
+    std::string path("/quote/AAPL/balance-sheet?p=AAPL");
+
+	Networking::HTTPSClient c(io_service, ctx, server, path);
+    io_service.run();
+    // ----
+
+
+    // [2] Get:
+    // ----
+	std::string _fileName("_HTTPSContent.txt");
+
+	// Test regex in file
+	std::ifstream file(_fileName);
+
+	std::string lineFromFile;
+	std::smatch match;
+
+	std::regex regexTotalEquityLine("Total Equity Gross Minority Interest.*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*)");
+	std::regex regexTotalDebtLine("Total Debt.*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*)");
+	std::regex regexSharesNumberLine("Share Issued.*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*)");
+
+	//
+	while (std::getline(file, lineFromFile)) 
+	{
+		// Get Total Equity
+		if (std::regex_search(lineFromFile, match, regexTotalEquityLine))
+		{
+			for (size_t i = 1; i < match.size(); ++i)
+		    {
+			    std::string totalEquityStr(match[i].str());
+			    // Remove "," from data
+			    totalEquityStr.erase(remove(totalEquityStr.begin(), totalEquityStr.end(), ','), totalEquityStr.end());
+
+			    double num = stod(totalEquityStr);
+			    std::cout << "Total Equity: " << num << '\n';
+			    // Push back in vector
+		    }
+		}
+		// Get Total Debt
+		if (std::regex_search(lineFromFile, match, regexTotalDebtLine))
+		{
+			for (size_t i = 1; i < match.size(); ++i)
+		    {
+			    std::string totalDebtStr(match[i].str());
+			    // Remove "," from data
+			    totalDebtStr.erase(remove(totalDebtStr.begin(), totalDebtStr.end(), ','), totalDebtStr.end());
+
+			    double num = stod(totalDebtStr);
+			    std::cout << "Total Debt: " << num << '\n';
+			    // Push back in vector
+		    }
+		}
+		// Get Number of Shares
+		if (std::regex_search(lineFromFile, match, regexSharesNumberLine))
+		{
+			for (size_t i = 1; i < match.size(); ++i)
+		    {
+			    std::string totalNumberOfSharesStr(match[i].str());
+			    // Remove "," from data
+			    totalNumberOfSharesStr.erase(remove(totalNumberOfSharesStr.begin(), totalNumberOfSharesStr.end(), ','), totalNumberOfSharesStr.end());
+
+			    double num = stod(totalNumberOfSharesStr);
+			    std::cout << "Shares Number: " << num << '\n';
+			    // Push back in vector
+		    }
+		}
+	}
+
+	// Close file
+	file.close();
+}
+
+
+
+TEST_F(NetworkingUnitTest, getFromCashFlowTest)
+{
+	// [1] Preparation
+	// ----
+	// Create client and get Income Statement (HTML) in _HTTPSContent.txt file
+	boost::asio::ssl::context ctx(boost::asio::ssl::context::sslv23);
+	ctx.set_default_verify_paths();
+
+    boost::asio::io_service io_service;
+
+    // Income Statement- APPLE
+    std::string server("finance.yahoo.com");
+    std::string path("/quote/AAPL/cash-flow?p=AAPL");
+
+	Networking::HTTPSClient c(io_service, ctx, server, path);
+    io_service.run();
+    // ----
+
+
+    // [2] Get:
+    // ----
+	std::string _fileName("_HTTPSContent.txt");
+
+	// Test regex in file
+	std::ifstream file(_fileName);
+
+	std::string lineFromFile;
+	std::smatch match;
+
+	std::regex regexFreeCashFlowLine("Free Cash Flow.*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*)");
+
+	//
+	while (std::getline(file, lineFromFile)) 
+	{
+		// Get Free Cash Flow
+		if (std::regex_search(lineFromFile, match, regexFreeCashFlowLine))
+		{
+			for (size_t i = 1; i < match.size(); ++i)
+		    {
+			    std::string freeCashFlowStr(match[i].str());
+			    // Remove "," from data
+			    freeCashFlowStr.erase(remove(freeCashFlowStr.begin(), freeCashFlowStr.end(), ','), freeCashFlowStr.end());
+
+			    double num = stod(freeCashFlowStr);
+			    std::cout << "Free Cash Flow: " << num << '\n';
+			    // Push back in vector
+		    }
+		}
+	}
+
+	// Close file
+	file.close();
+}
+
+
+
+/*
+TEST_F(NetworkingUnitTest, DISABLED_firstMyClassTest)
 {
 	// HTTPS (SSL) Clinet example
 	// https://github.com/alexandruc/SimpleHttpsClient/blob/master/https_client.cpp
@@ -66,8 +324,6 @@ TEST_F(NetworkingUnitTest, firstMyClassTest)
 
 TEST_F(NetworkingUnitTest, DISABLED_testGetRevenue)
 {
-	// Service::HTTPSProxySrv httpsProxySrvTemp("Test", "Test");
-
 	// String to parse	
 	std::string lineToRegex = "undefined\" title=\"Total Revenue\"><button aria-label=\"Total Revenue\" class=\"P(0) M(0) Va(m) Bd(0) Fz(s) Mend(2px) tgglBtn\"><svg class=\"H(16px) Fill($primaryColor) Stk($primaryColor) tgglBtn:h_Fill($linkColor) tgglBtn : h_Stk($linkColor) Cur(p)\" width=\"16\" style=\"stroke - width : 0; vertical - align:bottom\" height=\"16\" viewBox=\"0 0 48 48\" data-icon=\"caret - right\"><path d=\"M33.447 24.102L20.72 11.375c - .78 - .78 - 2.048 - .78 - 2.828 0 - .78.78 - .78 2.047 0 2.828l9.9 9.9 - 9.9 9.9c - .78.78 - .78 2.047 0 2.827.78.78 2.047.78 2.828 0l12.727 - 12.728z\"></path></svg></button><span class=\"Va(m)\">Total Revenue</span></div><div class=\"W(3px) Pos(a) Start(100 % ) T(0) H(100 % ) Bg($pfColumnFakeShadowGradient) Pe(n) Pend(5px)\"></div></div><div class=\"Ta(c) Py(6px) Bxz(bb) BdB Bdc($seperatorColor) Miw(120px) Miw(100px)--pnclg Bgc($lv1BgColor) fi - row : h_Bgc($hoverBgColor) D(tbc)\" data-test=\"fin - col\"><span>394,328,000</span></div><div class=\"Ta(c) Py(6px) Bxz(bb) BdB Bdc($seperatorColor) Miw(120px) Miw(100px)--pnclg D(tbc)\" data-test=\"fin - col\"><span>394,328,000</span></div><div class=\"Ta(c) Py(6px) Bxz(bb) BdB Bdc($seperatorColor) Miw(120px) Miw(100px)--pnclg Bgc($lv1BgColor) fi - row : h_Bgc($hoverBgColor) D(tbc)\" data-test=\"fin - col\"><span>365,817,000</span></div><div class=\"Ta(c) Py(6px) Bxz(bb) BdB Bdc($seperatorColor) Miw(120px) Miw(100px)--pnclg D(tbc)\" data-test=\"fin - col\"><span>274,515,000</span></div><div class=\"Ta(c) Py(6px) Bxz(bb) BdB Bdc($seperatorColor) Miw(120px) Miw(100px)--pnclg Bgc($lv1BgColor) fi - row : h_Bgc($hoverBgColor) D(tbc)\" data-test=\"fin - col\"><span>260,174,000</span></div></div><div></div></div><div class=\"\"";
 
@@ -90,7 +346,7 @@ TEST_F(NetworkingUnitTest, DISABLED_testGetRevenue)
 }
 
 
-TEST_F(NetworkingUnitTest, regexTest)
+TEST_F(NetworkingUnitTest, DISABLED_regexTest)
 {
 	std::string _fileName("_HTTPSContent.txt");
 
@@ -101,7 +357,7 @@ TEST_F(NetworkingUnitTest, regexTest)
 	std::smatch match;
 	// std::regex regexLine("\"revenue\":.\"raw\":(\\d+)");
 	// std::regex regexLine("\"annualFreeCashFlow\".*?raw\":(\\d+).*?raw\":(\\d+).*?raw\":(\\d+).*?raw\":(\\d+)");
-	std::regex regexLine("Total Revenue.*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*)");
+	std::regex regexLine("Net Income Common Stockholders.*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*).*?<span>([-]?[0-9]+(,[0-9]+)*)");
 
 
 	if (file.is_open()) 
@@ -119,12 +375,12 @@ TEST_F(NetworkingUnitTest, regexTest)
 			std::cout << "Regex match!" << '\n';
 			// wantedString = match.str(1); // match.str(0) - Whole matched string
 			// std::cout << "match.str(0): " << match.str(0) << '\n';
-			/*
+			
 			std::cout << "match.str(1): " << match.str(1) << '\n';
 			std::cout << "match.str(2): " << match.str(2) << '\n';
 			std::cout << "match.str(3): " << match.str(3) << '\n';
 			std::cout << "match.str(4): " << match.str(4) << '\n';
-			*/
+			
 
 			for (size_t i = 1; i < match.size(); ++i)
 		    {
@@ -140,6 +396,7 @@ TEST_F(NetworkingUnitTest, regexTest)
 	// Close file
 	file.close();	
 }
+
 
 
 TEST_F(NetworkingUnitTest, DISABLED_testReadHTTMLContent)
@@ -160,7 +417,7 @@ TEST_F(NetworkingUnitTest, DISABLED_testReadHTTMLContent)
 	}
 }
 
-/*
+
 TEST_F(NetworkingUnitTest, DISABLED_testWriteInFile)
 {
 	std::string _fileName("TestFile.txt");
